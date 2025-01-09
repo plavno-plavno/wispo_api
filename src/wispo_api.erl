96068ac7bd5cacc12a216eb0a3953cdfa34a274e
aleksandr.bo <aleksandr.bo@plavno.io>
2025-01-09 15:40:50 +0200

    Remove JSON-RPC. Add HTTP REST

--- src/wispo_api.erl
+++ src/wispo_api.erl
@@ -1,6 +1,10 @@
 -module(wispo_api).
 
 -export([
+    handle_call/3
+]).
+
+-export([
     reg_phone/1,
     reg_phone/2,
     confirm_phone/2
@@ -13,13 +17,42 @@
 
 -include("wispo_api_ets.hrl").
 
+-type path() :: binary().
+-export_type([path/0]).
+
 -type phone() :: binary().
 -export_type([phone/0]).
 
 -type code() :: list(non_neg_integer()).
 -export_type([code/0]).
 
-%% wispo_api:reg_phone(<<"+1234567890">>).
+-spec handle_call(path(), map(), map()) ->
+    {ok, map()}
+    | {error, map()}.
+handle_call(<<"/phones/register">>, #{<<"phone">> := Phone}, _State) ->
+    % TODO: #{<<"debug">> => true, <<"debug_key">> => <<"secret-id">>} (for CT)
+    reply(reg_phone(Phone));
+handle_call(<<"/phones/confirm">>, #{<<"phone">> := Phone, <<"code">> := Code}, _State) ->
+    reply(confirm_phone(Phone, Code));
+handle_call(<<"/health/check">>, _Data, _State) ->
+    reply(ok);
+handle_call(_Path, _Data, _State) ->
+    reply({error, unexpected_call}).
+
+%%% Internal functions
+
+%% @private
+-spec reply(atom() | tuple()) -> map().
+reply(ok) ->
+    #{status => ok};
+reply({ok, Data}) ->
+    #{status => ok, data => Data};
+reply({error, Reason}) ->
+    #{status => error, data => Reason};
+reply(_Any) ->
+    #{status => error, data => internal_error}.
+
+%% @private
 -spec reg_phone(binary()) ->
     {ok, map()}
     | {error, term()}.
@@ -27,7 +60,7 @@ reg_phone(Phone) ->
     Config = wispo_api_config:get(wispo_api, auth_code),
     reg_phone(Phone, Config).
 
-%% wispo_api:reg_phone(<<"+1234567890">>, [{code_ttl, 60}]).
+%% @private
 -spec reg_phone(phone(), map()) ->
     {ok, map()}
     | {error, term()}.
@@ -46,7 +79,7 @@ reg_phone(Phone, Opts) ->
         code_ttl => CodeTtl
     }}.
 
-%% wispo_api:confirm_phone(<<"+1234567890">>, [6,5,4,6])
+%% @private
 -spec confirm_phone(phone(), code()) -> ok | {error, expired}.
 confirm_phone(Phone, Code) ->
     case ets:lookup(?ETS_NAME, Phone) of
@@ -61,7 +94,7 @@ confirm_phone(Phone, Code) ->
             {error, mismatch}
     end.
 
-%% wispo_api:is_known_phone(<<"+1234567890">>).
+%% @private
 -spec is_known_phone(phone()) -> boolean().
 is_known_phone(Phone) ->
     case ets:lookup(?ETS_NAME, Phone) of
@@ -71,7 +104,7 @@ is_known_phone(Phone) ->
             false
     end.
 
-%% wispo_api:is_confirmed_phone(<<"+1234567890">>).
+%% @private
 -spec is_confirmed_phone(phone()) -> boolean().
 is_confirmed_phone(Phone) ->
     case ets:lookup(?ETS_NAME, Phone) of
