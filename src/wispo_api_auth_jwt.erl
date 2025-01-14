e9f40c36773dfa7b6d50592ab74056fbca9545fa
aleksandr.bo <aleksandr.bo@plavno.io>
2025-01-14 06:20:54 +0200

    Add JWT refresh generation

--- src/wispo_api_auth_jwt.erl
+++ src/wispo_api_auth_jwt.erl
@@ -7,7 +7,10 @@
   verify/1
 ]).
 
--define(DEFAULT_JWT_TTL, 60 * 2).
+-include("wispo_api_common_utils.hrl").
+
+-define(DEFAULT_JWT_TTL, 60 * 5).
+-define(DEFAULT_JWT_REFRESH_TTL, ?SECONDS_PER_DAY * 365).
 
 -spec generate(map()) -> map().
 generate(Payload) ->
@@ -19,20 +22,27 @@ generate(Payload, Opts) ->
   Alg = proplists:get_value(alg, Opts),
   Key = proplists:get_value(key, Opts),
   Iss = proplists:get_value(iss, Opts),
-  Ttl = proplists:get_value(jwt_access_ttl, Opts, ?DEFAULT_JWT_TTL),
-  Exp = erlang:system_time(seconds) + Ttl,
+  TtlA = proplists:get_value(jwt_access_ttl, Opts, ?DEFAULT_JWT_TTL),
+  TtlR = proplists:get_value(jwt_refresh_ttl, Opts, ?DEFAULT_JWT_REFRESH_TTL),
+  ExpA = erlang:system_time(seconds) + TtlA,
+  ExpR = erlang:system_time(seconds) + TtlR,
   Jwk = #{<<"kty">> => <<"oct">>, <<"k">> => Key},
   Jws = #{<<"alg">> => Alg},
   Jwt = #{
     <<"iss">> => Iss,
-    <<"exp">> => Exp
+    <<"exp">> => ExpA
   },
   Signed = jose_jwt:sign(Jwk, Jws, maps:merge(Jwt, Payload)),
   {_, AccessJwt} = jose_jws:compact(Signed),
+  RefreshJwt = maps:put(<<"exp">>, ExpR, Jwt),
+  RefreshJwt2 = maps:put(<<"is_refresh">>, true, RefreshJwt),
+  SignedRefresh = jose_jwt:sign(Jwk, Jws, maps:merge(RefreshJwt2, Payload)),
+  {_, RefreshJwt3} = jose_jws:compact(SignedRefresh),
   #{
     access_jwt => AccessJwt,
-    refresh_jwt => null,
-    expires_in => Exp
+    access_jwt_expires_in => ExpA,
+    refresh_jwt => RefreshJwt3,
+    refresh_jwt_expires_in => ExpR
   }.
 
 -spec validate(binary()) -> ok.
@@ -43,5 +53,4 @@ validate(_) ->
 verify(_) ->
   {error, not_implemented}.
 
-%% TODO: pubkey_rotate
 
