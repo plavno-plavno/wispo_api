4a6c1783aa20532a71459f58871990b6161a1e15
aleksandr.bo <aleksandr.bo@plavno.io>
2025-01-15 17:08:44 +0200

    Time utils

--- src/wispo_api_time_utils.erl
+++ src/wispo_api_time_utils.erl
@@ -1,7 +1,9 @@
 -module(wispo_api_time_utils).
 
 -export([
-    is_valid_unixtime/1
+    is_valid_unixtime/1,
+    unixtime_to_localtime/1,
+    unixtime_to_rfc1123/1
 ]).
 
 -include("wispo_api_common_utils.hrl").
@@ -12,4 +14,11 @@ is_valid_unixtime(X) when is_integer(X) ->
 is_valid_unixtime(_X) ->
     false.
 
+-spec unixtime_to_localtime(pos_integer()) -> calendar:datetime().
+unixtime_to_localtime(UnixTime) ->
+    calendar:system_time_to_local_time(UnixTime, seconds).
 
+-spec unixtime_to_rfc1123(pos_integer()) -> binary().
+unixtime_to_rfc1123(UnixTime) ->
+    LocalTime = unixtime_to_localtime(UnixTime),
+    list_to_binary(httpd_util:rfc1123_date(LocalTime)).
