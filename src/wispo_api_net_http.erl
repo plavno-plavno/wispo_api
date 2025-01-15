b58239b2131c15a7c8c4db6de981548d377d72cb
aleksandr.bo <aleksandr.bo@plavno.io>
2025-01-15 18:31:37 +0200

    Add Bearer authentiction for API

--- src/wispo_api_net_http.erl
+++ src/wispo_api_net_http.erl
@@ -77,30 +77,19 @@ stop() ->
 %%% Cowboy callbacks
 %%% ==================================================================
 
-%% -------------------------------------------------------------------
-%% @private
-%% -------------------------------------------------------------------
 -spec init(Req, any()) -> {ok | module(), Req, any()} | {module(), Req, any(), any()} when
   Req :: cowboy_req:req().
-
 init(Req, Opts) ->
   State = init_state(Req, Opts),
   {cowboy_rest, Req, State}.
 
-%% -------------------------------------------------------------------
-%% @private
-%% -------------------------------------------------------------------
 -spec terminate(Reason, Req, State) -> ok when
   Reason :: normal | {crash, error | exit | throw, any()},
   Req :: cowboy_req:req(),
   State :: term().
-
 terminate(_Reason, _Req, _State) ->
   ok.
 
-%% -------------------------------------------------------------------
-%% @private
-%% -------------------------------------------------------------------
 -spec allowed_methods(Req, State) -> {Result, Req, State} when
   Req :: cowboy_req:req(),
   State :: term(),
@@ -108,62 +97,62 @@ terminate(_Reason, _Req, _State) ->
   Method :: binary(),
   Req :: cowboy_req:req(),
   State :: term().
-
 allowed_methods(Req, State) ->
   Methods = [<<"GET">>, <<"POST">>, <<"PUT">>, <<"DELETE">>],
   {Methods, Req, State}.
 
-%% -------------------------------------------------------------------
-%% @private
-%% -------------------------------------------------------------------
 -spec allow_missing_post(Req, State)
       -> {boolean(), Req, State}
     | {stop, Req, State}
     | {cowboy_rest:switch_handler(), Req, State}
   when Req::cowboy_req:req(), State::any().
-
 allow_missing_post(Req, State) ->
   {false, Req, State}.
 
-%% -------------------------------------------------------------------
-%% @private
-%% -------------------------------------------------------------------
+%% @reference https://www.rfc-editor.org/rfc/rfc6750
 -spec is_authorized(Req, State) ->
   {true | {false, iodata()}, Req, State}
   | {stop, Req, State}
   | {cowboy_rest:switch_handler(), Req, State}
   when Req::cowboy_req:req(), State::any().
-
 is_authorized(Req, State) ->
-  {true, Req, State}.
+  Config = wispo_api_config:get(wispo_api, auth_bearer),
+  L = proplists:get_value(turn_on_for_uris, Config, []),
+  P = path(Req),
+  case lists:member(P, L) of
+    true ->
+      case cowboy_req:parse_header(<<"authorization">>, Req) of
+        {bearer, AccessJwt} ->
+          Result = wispo_api_auth_jwt:verify(AccessJwt) and (not wispo_api_auth_jwt:is_jwt_refresh(AccessJwt)),
+          case Result of
+            true ->
+              {true, Req, State};
+            false ->
+              {{false, <<"Bearer realm=\"wispo\", error=\"invalid_token\", error_description=\"The access token expired\"">>}, Req, State}
+          end;
+        _ ->
+          {{false, <<"Bearer realm=\"wispo\", error=\"invalid_token\", error_description=\"Bearer token required\"">>}, Req, State}
+      end;
+    false ->
+      {true, Req, State}
+  end.
 
-%% -------------------------------------------------------------------
-%% @private
-%% -------------------------------------------------------------------
 -spec forbidden(Req, State)
       -> {boolean(), Req, State}
     | {stop, Req, State}
     | {cowboy_rest:switch_handler(), Req, State}
   when Req::cowboy_req:req(), State::any().
-
 forbidden(Req, State) ->
   {false, Req, State}.
 
-%% -------------------------------------------------------------------
-%% @private
-%% -------------------------------------------------------------------
 -spec service_available(Req, State)
       -> {boolean(), Req, State}
     | {stop, Req, State}
     | {cowboy_rest:switch_handler(), Req, State}
   when Req::cowboy_req:req(), State::any().
-
 service_available(Req, State) ->
   {true, Req, State}.
 
-%% -------------------------------------------------------------------
-%% @private
-%% -------------------------------------------------------------------
 -spec content_types_accepted(Req, State) -> {Result, Req, State} when
   Req :: cowboy_req:req(),
   State :: term(),
@@ -172,15 +161,11 @@ service_available(Req, State) ->
   Params :: [{Key :: binary(), Value :: binary()}],
   Req :: cowboy_req:req(),
   State :: term().
-
 content_types_accepted(Req, State) ->
   {[
     {<<"application/json">>, from_json}
   ], Req, State}.
 
-%% -------------------------------------------------------------------
-%% @private
-%% -------------------------------------------------------------------
 -spec content_types_provided(Req, State) -> {Result, Req, State} when
   Req :: cowboy_req:req(),
   State :: term(),
@@ -189,24 +174,18 @@ content_types_accepted(Req, State) ->
   Params :: [{Key :: binary(), Value :: binary()}],
   Req :: cowboy_req:req(),
   State :: term().
-
 content_types_provided(Req, State) ->
   {[
     {<<"application/json">>, to_json}
   ], Req, State}.
 
-%% -------------------------------------------------------------------
-%% @private
-%% -------------------------------------------------------------------
 -spec from_json(Req, State) -> {Result, Req, State} when
   Req :: cowboy_req:req(),
   State :: term(),
   Result :: cowboy_req:resp_body().
-
-% cowboy_req:set_resp_cookie(<<"t">>, Jwt, Req1, #{path => <<"/">>});
 from_json(Req, State) ->
   State2 = init_state(Req, State),
-  <<"/rest/1", Path/binary>> = cowboy_req:path(Req),
+  Path = path(Req),
   ReqBody = get_req_body(Req, State2),
   RespBody = wispo_api:handle_call(Path, ReqBody, State2),
   Json = jsx:encode(RespBody),
@@ -214,17 +193,13 @@ from_json(Req, State) ->
   Req2 = cowboy_req:reply(?HTTP_200_OK, Headers, Json, Req),
   {stop, Req2, State2}.
 
-%% -------------------------------------------------------------------
-%% @private
-%% -------------------------------------------------------------------
 -spec to_json(Req, State) -> {Result, Req, State} when
   Req :: cowboy_req:req(),
   State :: term(),
   Result :: cowboy_req:resp_body().
-
 to_json(Req, State) ->
   State2 = init_state(Req, State),
-  <<"/rest/1", Path/binary>> = cowboy_req:path(Req),
+  Path = path(Req),
   ReqBody = null,
   RespBody = wispo_api:handle_call(Path, ReqBody, State2),
   Json = jsx:encode(RespBody),
@@ -232,41 +207,34 @@ to_json(Req, State) ->
   Req2 = cowboy_req:reply(?HTTP_200_OK, Headers, Json, Req),
   {stop, Req2, State2}.
 
-%% -------------------------------------------------------------------
-%% @private
-%% -------------------------------------------------------------------
 -spec init_state(Req, State) -> {ApiCall, Ctx} when
   Req :: cowboy_req:req(),
   State :: maps:map(),
   ApiCall :: maps:map(),
   Ctx :: maps:map().
-
-init_state(Req, #{api := http_rest} = State) ->
-  init_state_rest(Req, State);
 init_state(Req, State) ->
   {Ip, _Port} = cowboy_req:peer(Req),
   State2 = #{
-    request_time  => erlang:system_time(seconds),
-    client_ip     => erlang:list_to_binary(inet:ntoa(Ip)),
-    protocol_vsn  => cowboy_req:binding(vsn, Req, 1)
+    client_ip => erlang:list_to_binary(inet:ntoa(Ip)),
+    req_time  => erlang:system_time(seconds),
+    api_vsn   => cowboy_req:binding(vsn, Req, 1),
+    op_name   => http_method_to_op_name(Req),
+    op_args   => cowboy_req:parse_qs(Req)
   },
   maps:merge(State, State2).
 
-init_state_rest(Req, State) ->
-  {Ip, _Port} = cowboy_req:peer(Req),
-  State2 = #{
-    request_time    => erlang:system_time(seconds),
-    client_ip       => erlang:list_to_binary(inet:ntoa(Ip)),
-    protocol_vsn    => cowboy_req:binding(vsn, Req, 1),
-    op_name         => http_method_to_op_name(Req),
-    op_args         => cowboy_req:parse_qs(Req),
-    jsonrpc_method  => uri_to_method_name(Req)
-  },
-  maps:merge(State, State2).
+-spec get_req_body(cowboy_req:req(), map()) ->
+  {ok, binary()}
+  | {error, nobody}.
+get_req_body(Req, _State) ->
+  case cowboy_req:method(Req) of
+    M when M =:= <<"GET">>; M =:= <<"DELETE">> ->
+      maps:from_list(cowboy_req:parse_qs(Req));
+    M when M =:= <<"PUT">>; M =:= <<"POST">> ->
+      get_req_body(Req);
+    _ -> undefined
+  end.
 
-%% -------------------------------------------------------------------
-%% @private
-%% -------------------------------------------------------------------
 -spec get_req_body(cowboy_req:req()) ->
   {ok, binary()}
   | {error, nobody}.
@@ -279,32 +247,20 @@ get_req_body(Req) ->
       {error, nobody}
   end.
 
--spec get_req_body(cowboy_req:req(), map()) ->
-  {ok, binary()}
-  | {error, nobody}.
-get_req_body(Req, #{api := http_rest} = _State) ->
-  case cowboy_req:method(Req) of
-    M when M =:= <<"GET">>; M =:= <<"DELETE">> ->
-      maps:from_list(cowboy_req:parse_qs(Req));
-    M when M =:= <<"PUT">>; M =:= <<"POST">> ->
-      get_req_body(Req);
-    _ -> undefined
-  end.
-
-%% @private
--spec http_method_to_op_name(cowboy_req:req()) -> binary() | undefined.
+-spec http_method_to_op_name(cowboy_req:req()) ->
+  binary()
+  | undefined.
 http_method_to_op_name(Req) ->
   case cowboy_req:method(Req) of
     <<"GET">> -> <<"read">>;
     <<"POST">> -> <<"create">>;
-    <<"PUT">> -> <<"update">>;
+    <<"PUT">> -> <<"upset">>;
+    <<"PATCH">> -> <<"update">>;
     <<"DELETE">> -> <<"delete">>;
     _ -> undefined
   end.
 
--spec uri_to_method_name(cowboy_req:req()) -> binary() | undefined.
-uri_to_method_name(Req) ->
-  Bindings = cowboy_req:bindings(Req),
-  ObjType = maps:get(obj_type, Bindings),
-  ObjId = maps:get(obj_id, Bindings),
-  <<ObjType/binary, ".", ObjId/binary>>.
+-spec path(cowboy_req:req()) -> binary().
+path(Req) ->
+  <<"/rest/1", Path/binary>> = cowboy_req:path(Req),
+  Path.
